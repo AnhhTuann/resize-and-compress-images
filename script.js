5491ee439f85ac6ff78fb89af958c1509279c8f2
AnhhTuann <tuannguyen25101999@gmail.com>
2023-04-16 04:56:20 +0700

    Showing height and width of an image

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const uploadBox = document.querySelector('.upload-box');
-reviewImg = uploadBox.querySelector('img');
-fileInput = uploadBox.querySelector('input');
+const uploadBox = document.querySelector('.upload-box'),
+  reviewImg = uploadBox.querySelector('img'),
+  fileInput = uploadBox.querySelector('input'),
+  widthInput = document.querySelector('.width input'),
+  heightInput = document.querySelector('.height input');
 
 const loadFile = (e) => {
   const file = e.target.files[0];
   if (!file) return;
   reviewImg.src = URL.createObjectURL(file);
   reviewImg.addEventListener('load', () => {
+    widthInput.value = reviewImg.naturalWidth;
+    heightInput.value = reviewImg.naturalHeight;
     document.querySelector('.wrapper').classList.add('active');
   });
 
