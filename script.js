3dfac178852546fe15eb36f584f8cfefa9233e2f
AnhhTuann <tuannguyen25101999@gmail.com>
2023-04-16 04:49:10 +0700

    Reviewing selected image

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const uploadBox = document.querySelector('.upload-box');
+reviewImg = uploadBox.querySelector('img');
 fileInput = uploadBox.querySelector('input');
 
+const loadFile = (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+  reviewImg.src = URL.createObjectURL(file);
+  reviewImg.addEventListener('load', () => {
+    document.querySelector('.wrapper').classList.add('active');
+  });
+
+  console.log(file);
+};
+
+fileInput.addEventListener('change', loadFile);
 uploadBox.addEventListener('click', () => {
   fileInput.click();
 });
